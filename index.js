09742430b9051211bc1bf2fa16029519d4d7c375
Wilson Andrés Camargo <49167124+wilsoncamgo@users.noreply.github.com>
2020-12-12 06:28:58 -0500

    working v2

--- index.js
+++ index.js
@@ -12,6 +12,55 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(cors())
+app.use(compression())
+app.use(helmet())
+const isProduction = process.env.NODE_ENV === 'production'
+const origin = {
+  origin: isProduction ? 'https://www.example.com' : '*',
+}
+
+app.use(cors(origin))
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minute
+    max: 5, // 5 requests,
+  })
+  
+  app.use(limiter)
+  const postLimiter = rateLimit({
+    windowMs: 1 * 60 * 1000,
+    max: 1,
+  })
+  
+  app.post('/users', postLimiter, addUser)
+
+  app.post(
+    '/users',
+    [
+      check('id').not().isEmpty().isLength({min: 5, max: 255}).trim(),
+      check('name').not().isEmpty().isLength({min: 5, max: 255}).trim(),
+    ],
+    postLimiter,
+    (request, response) => {
+      const errors = validationResult(request)
+  
+      if (!errors.isEmpty()) {
+        return response.status(422).json({errors: errors.array()})
+      }
+  
+      const {id, name} = request.body
+  
+      pool.query(
+        'INSERT INTO users (id, name) VALUES ($1, $2)',
+        [id, name],
+        (error) => {
+          if (error) {
+            throw error
+          }
+          response.status(201).json({status: 'success', message: 'User added.'})
+        },
+      )
+    },
+  )
 
 const getUsers = (request, response) => {
   pool.query('SELECT * FROM users', (error, results) => {
