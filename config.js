afd6630eff6850188635f4a626cb6a3e5a430682
Wilson Andrés Camargo <49167124+wilsoncamgo@users.noreply.github.com>
2021-01-03 08:01:30 -0500

    added full support

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 
-const {Pool} = require('pg')
+const { Pool } = require('pg')
 const isProduction = process.env.NODE_ENV === 'production'
 
 const connectionString = `postgresql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_DATABASE}`
@@ -9,5 +9,40 @@ const pool = new Pool({
   connectionString: isProduction ? process.env.DATABASE_URL : connectionString,
   ssl: isProduction,
 })
+const getUsers = (request, response) => {
+  pool.query("SELECT * FROM users", (error, results) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
+const getUserbyId = (request, response) => {
+  const { id } = request.body;
+  pool.query("SELECT name FROM users WHERE id=$1", [id], (error, res) => {
+    if (error) {
+      throw error;
+    }
+    response.status(200).json(res.rows);
+  });
+};
 
-module.exports = {pool}
+const addUser = (request, response) => {
+  const { id, name } = request.body;
+
+  pool.query(
+    "INSERT INTO users (id, name) VALUES ($1, $2)",
+    [id, name],
+    (error) => {
+      if (error) {
+        throw error;
+      }
+      response.status(201).json({ status: "success", message: "User added." });
+    }
+  );
+};
+module.exports = {
+  getUsers,
+  getUserbyId,
+  addUser
+}
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const bodyParser = require("body-parser");
 const cors = require("cors");
 const morgan = require("morgan");
 const helmet = require("helmet");
-const { pool } = require("./config");
+const db = require("./config");
 const compression = require("compression");
 const app = express();
 
@@ -21,44 +21,9 @@ const origin = {
 
 app.use(cors(origin));
 
-const getUsers = (request, response) =>  {
-  pool.query("SELECT * FROM users", (error, results) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(results.rows);
-  });
-};
-const getUserbyId = (request, response) => {
-  const { id } = request.body;
-  pool.query("SELECT name FROM users WHERE id=$1", [id], (error, res) => {
-    if (error) {
-      throw error;
-    }
-    response.status(200).json(res.rows);
-  });
-};
-
-const addUser = (request, response) => {
-  const { id, name } = request.body;
-
-  pool.query(
-    "INSERT INTO users (id, name) VALUES ($1, $2)",
-    [id, name],
-    (error) => {
-      if (error) {
-        throw error;
-      }
-      response.status(201).json({ status: "success", message: "User added." });
-    }
-  );
-};
-
-app
-  .route("/users")
-  .get(getUsers)
-  .post(addUser);
-  app.route("/users/finder").get(getUserbyId);
+app.get('/users', db.getUsers)
+app.get('/users/:id',db.getUserbyId)
+app.post('/users', db.addUser)
 
 // Start server
 app.listen(process.env.PORT || 3002, () => {
