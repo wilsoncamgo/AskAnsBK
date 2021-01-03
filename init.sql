8d2ad3f754c7637f7ceab99b2b9cc5efa2b62ba7
Wilson Andrés Camargo <49167124+wilsoncamgo@users.noreply.github.com>
2021-01-03 18:52:54 -0500

    added fixes to database

--- init.sql
+++ init.sql
@@ -3,11 +3,20 @@ CREATE TABLE users(
     name VARCHAR(255) NOT NULL
 );
 
+CREATE TABLE topics(
+    id SERIAL PRIMARY KEY,
+    topic VARCHAR(50) NOT NULL
+);
+
 CREATE TABLE questions(
     id SERIAL PRIMARY KEY,
     user_id VARCHAR(30) NOT NULL UNIQUE,
+    topic_id INT NOT NULL,
     question VARCHAR(500) NOT NULL,
     date_sumb DATE NOT NULL,
+    FOREIGN KEY (topic_id)
+        REFERENCES topic(id)
+        ON UPDATE RESTRICT,
     FOREIGN KEY(user_id)
         REFERENCES users(id)
         ON UPDATE RESTRICT
@@ -16,14 +25,21 @@ CREATE TABLE questions(
 
 CREATE TABLE answers(
     id SERIAL PRIMARY KEY,
+    question_id INT NOT NULL,
     user_id VARCHAR(30) NOT NULL UNIQUE,
     answer VARCHAR(500) NOT NULL,
     date_sumb DATE NOT NULL,
+    FOREIGN KEY(question_id)
+        REFERENCES questions(id)
+        ON UPDATE RESTRICT
+        ON DELETE RESTRICT,
     FOREIGN KEY(user_id)
         REFERENCES users(id)
         ON UPDATE RESTRICT
         ON DELETE RESTRICT
 );
+
+
 
 INSERT INTO users (id,name)
 VALUES (1000948840, 'Wilson Andres');
